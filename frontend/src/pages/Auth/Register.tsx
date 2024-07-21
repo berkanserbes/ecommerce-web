2598668e952ebadbcaac74e86cfb02b4ed036ad1
Berkan Serbes <berkan_serbes@hotmail.com>
2024-07-21 18:11:17 +0300

    Add register functionality, used formik vor validation

--- frontend/src/pages/Auth/Register.tsx
+++ frontend/src/pages/Auth/Register.tsx
@@ -1,4 +1,52 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+interface IInitialValues {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
 const Register: React.FC = () => {
+  const initialValues: IInitialValues = {
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Required"),
+    surname: Yup.string().required("Required"),
+    email: Yup.string().email("Invalid email address").required("Required"),
+    password: Yup.string().required("Required"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password"), undefined], "Passwords must match")
+      .required("Required"),
+  });
+
+  const handleSubmit = async (
+    values: IInitialValues,
+    { resetForm }: { resetForm: () => void }
+  ) => {
+    try {
+      const result = await axios.post(
+        "http://localhost:4000/api/authentication/register",
+        values
+      );
+      console.log(result);
+      // Formu sıfırla
+      resetForm();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="flex flex-col md:flex-row h-screen">
       {/* Image Column */}
@@ -14,87 +62,126 @@ const Register: React.FC = () => {
             Create an account
           </h2>
           <div className="bg-white p-6 rounded-lg shadow-md">
-            <form>
-              <div className="mb-4 flex flex-col md:flex-row md:space-x-4">
-                <div className="flex-1 mb-4 md:mb-0">
-                  <label
-                    htmlFor="name"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Name
-                  </label>
-                  <input
-                    id="name"
-                    name="name"
-                    type="text"
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  />
-                </div>
-                <div className="flex-1">
-                  <label
-                    htmlFor="surname"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Surname
-                  </label>
-                  <input
-                    id="surname"
-                    name="surname"
-                    type="text"
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
+            <Formik
+              initialValues={initialValues}
+              resetForm
+              onSubmit={handleSubmit}
+              validationSchema={validationSchema}
+            >
+              {({ isValid, dirty }) => (
+                <Form>
+                  <div className="mb-4 flex flex-col md:flex-row md:space-x-4">
+                    <div className="flex-1 mb-4 md:mb-0">
+                      <label
+                        htmlFor="name"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Name
+                      </label>
+                      <Field
+                        id="name"
+                        name="name"
+                        type="text"
+                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      />
+                      <ErrorMessage
+                        name="name"
+                        component="div"
+                        className="text-red-600 text-sm"
+                      />
+                    </div>
+                    <div className="flex-1">
+                      <label
+                        htmlFor="surname"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Surname
+                      </label>
+                      <Field
+                        id="surname"
+                        name="surname"
+                        type="text"
+                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      />
+                      <ErrorMessage
+                        name="surname"
+                        component="div"
+                        className="text-red-600 text-sm"
+                      />
+                    </div>
+                  </div>
 
-              <div className="mb-4">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Email
-                </label>
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-              </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-              </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="confirmPassword"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Confirm Password
-                </label>
-                <input
-                  id="confirmPassword"
-                  name="confirmPassword"
-                  type="password"
-                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-              </div>
-              <button
-                type="submit"
-                className="w-full py-2 px-4 bg-indigo-600 text-white font-bold rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                Register
-              </button>
-            </form>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Email
+                    </label>
+                    <Field
+                      id="email"
+                      name="email"
+                      type="email"
+                      className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      className="text-red-600 text-sm"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Password
+                    </label>
+                    <Field
+                      id="password"
+                      name="password"
+                      type="password"
+                      className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                    <ErrorMessage
+                      name="password"
+                      component="div"
+                      className="text-red-600 text-sm"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="confirmPassword"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Confirm Password
+                    </label>
+                    <Field
+                      id="confirmPassword"
+                      name="confirmPassword"
+                      type="password"
+                      className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                    <ErrorMessage
+                      name="confirmPassword"
+                      component="div"
+                      className="text-red-600 text-sm"
+                    />
+                  </div>
+                  <button
+                    disabled={!isValid || !dirty}
+                    type="submit"
+                    className={`w-full py-2 px-4 font-bold rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 ${
+                      !isValid || !dirty
+                        ? "bg-[#e2e0ff] text-[#8881f8] cursor-not-allowed"
+                        : "bg-indigo-600 text-white hover:bg-indigo-700 focus:ring-indigo-500"
+                    }`}
+                  >
+                    Register
+                  </button>
+                </Form>
+              )}
+            </Formik>
             <div>
               <p className="mt-4 text-sm text-center">
                 Already have an account?{" "}
