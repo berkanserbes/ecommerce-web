aa6139c275c43c51fdfe85a92aa850b9dc54b1da
Berkan Serbes <berkan_serbes@hotmail.com>
2024-07-21 19:33:50 +0300

    İmplement private route handling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,26 @@
-import CustomButton from "@components/CustomButton";
-import "./App.css";
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Register from "@pages/Auth/Register";
+import Login from "@pages/Auth/Login";
+import PrivateRoute from "./utils/router/PrivateRoute";
+import Home from "@pages/Home/Home";
 
-function App() {
+const App: React.FC = () => {
+  // Burada kullanıcı giriş durumunu kontrol eden bir state olabilir
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
-    <>
-      <div>
-        <CustomButton />
-      </div>
-    </>
+    <Routes>
+      <Route element={<PrivateRoute isAuthenticated={isAuthenticated} />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+      <Route path="/register" element={<Register />} />
+      <Route
+        path="/login"
+        element={<Login setIsAuthenticated={setIsAuthenticated} />}
+      />
+    </Routes>
   );
-}
+};
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,23 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Register from "./pages/Auth/Register.tsx";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-]);
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
