ca2dfcd658ea59260de9c845173fdf336106c77b
Berkan Serbes <berkan_serbes@hotmail.com>
2024-07-21 18:11:48 +0300

    Add Cors npm package

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const connectDB = require("./configs/db");
 const { registerRoute, loginRoute, productRoute } = require("./routes");
 const authenticateMiddleware = require("./middlewares/authenticate");
+
+app.use(cors());
 
 app.use(express.json());
 
