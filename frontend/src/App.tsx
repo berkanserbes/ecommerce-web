f4828cdc0da5eb2579c5e1e6a283c9b42975376e
Berkan Serbes <berkan_serbes@hotmail.com>
2024-07-29 19:52:13 +0300

    feat: Add admin dashboard route and component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,21 +3,39 @@ import { Route, Routes } from "react-router-dom";
 import Register from "@pages/Auth/Register";
 import Login from "@pages/Auth/Login";
 import PrivateRoute from "./utils/router/PrivateRoute";
+import AdminRoute from "./utils/router/AdminRoute";
+import AdminDashboard from "@pages/Admin/AdminDasboard";
 import Home from "@pages/Home/Home";
 
 const App: React.FC = () => {
-  // Burada kullanıcı giriş durumunu kontrol eden bir state olabilir
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [isAdmin, setIsAdmin] = useState<boolean>(false);
 
   return (
     <Routes>
       <Route element={<PrivateRoute isAuthenticated={isAuthenticated} />}>
         <Route path="/" element={<Home />} />
       </Route>
+      <Route
+        element={
+          <AdminRoute
+            isAuthenticated={isAuthenticated}
+            isAdmin={isAdmin}
+            setIsAdmin={setIsAdmin}
+          />
+        }
+      >
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+      </Route>
       <Route path="/register" element={<Register />} />
       <Route
         path="/login"
-        element={<Login setIsAuthenticated={setIsAuthenticated} />}
+        element={
+          <Login
+            setIsAuthenticated={setIsAuthenticated}
+            setIsAdmin={setIsAdmin}
+          />
+        }
       />
     </Routes>
   );
